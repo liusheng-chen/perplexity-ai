0ddebce79d7dbeb5a0a9b25d556af0d90233d0cf
liusheng.chen <liusheng.chen@denglin.ai>
2025-06-13 13:29:03 +0800

    update

---
+++ test.py
@@ -0,0 +1,56 @@
+import perplexity
+
+perplexity_cookies = { 
+    'pplx.visitor-id': 'c5578011-24f3-40b4-88f9-35e1a682de1c',
+    '_gcl_au': '1.1.2135055386.1746518844',
+    'g_state': '{"i_l":0}',
+    'intercom-device-id-l2wyozh0': 'a7406585-aa42-4e16-b999-22ff90b073d8',
+    '__stripe_mid': '1adba61c-1ec8-4a08-a3b4-ceb6891e726831616b',
+    'ph_phc_TXdpocbGVeZVm5VJmAsHTMrCofBQu3e0kN8HGMNGTVW_posthog': '%7B%22distinct_id%22%3A%220196a4b6-8e60-7769-a70b-c1fa73135e07%22%2C%22%24sesid%22%3A%5B1746520149672%2C%220196a4b6-8e5e-72db-bddc-11dc62125f6e%22%2C1746520149598%5D%7D',
+    '__podscribe_perplexityai_referrer': '_',
+    '__podscribe_perplexityai_landing_url': 'https://www.perplexity.ai/account/api/keys?__cf_chl_tk=5TjfYAp4qAipB0zFnUPTxos7Cvogo0K.sRQdqWe3klw-1746520100-1.0.1.1-WCJX5Gu.87T9cAY1uJ90GrB0xK0yAI17HZOhTt_XRqo',
+    'pplx.search-models': '{%22search%22:%22claude37sonnetthinking%22%2C%22research%22:%22pplx_alpha%22}',
+    'pplx.side-upsell-enterprise-dismissed': 'true',
+    'pplx.chosen-locale': 'en-US',
+    'pplx.search-mode': 'search',
+    'pplx.search-models-v2': '{%22studio%22:%22pplx_beta%22%2C%22search%22:%22pplx_pro%22%2C%22research%22:%22pplx_alpha%22}',
+    'segmented-control-popover-studio': '5',
+    'sidebarHiddenHubs': '[]',
+    'isSidebarPinned': 'true',
+    'pplx.session-id': '69e2155f-58a7-414b-8d2d-0a2c2d021350',
+    '__cflb': '02DiuDyvFMmK5p9jVbVnMNSKYZhUL9aGkZAiiNmGKzXDn',
+    'next-auth.csrf-token': '5a82b168ba0553aed31a2170f7bc8c753f6c7560fda9190b7532f71d3f379127%7C8d3fedc25379aeedee185256a0116ad3b258d9cf0a458ea3ce502cacc0c7e029',
+    'next-auth.callback-url': 'https%3A%2F%2Fwww.perplexity.ai%2Fapi%2Fauth%2Fsignin-callback%3Fredirect%3Dhttps%253A%252F%252Fwww.perplexity.ai',
+    '_rdt_uuid': '1746520755506.c44bd770-fae6-4fa2-ac6e-fe4f105c977b',
+    '__cf_bm': '6TceRpHy4VQlynJdORmXPj06JJMAjOxhgoCZtC.EPW8-1749791275-1.0.1.1-NXjsWa98UNNtBhw2HQ5Rydhs5Wds8ZFl1zKbd5A8b3SquN632fiI3ckhq1B8IV4IJXKfqgf6CXC8e5DWx44uanAzx5mNs0KPqwDY1P_0ewQ',
+    'cf_clearance': 'zIeyaLPmcPGFwA4OQAOZ9zsAMbcVKUjeOwHOMMUvpYc-1749791278-1.2.1.1-5fC3W3kLyPiWh5mn3zwBlXbzIZhV.5utEtB1pCY2H5ofQLefOpQISao5iohXrw56SB.VnvLUYasqbkdSg_38GMTGyBoNW1If9C5EIlAAcX2PJRzzDmH4LsXBuYA9tQNJ2CSuVZj4hWE6mxFUiagB1XQpWvbK2D7hOREQ9YS9VyTAgqjQSCGlDvEMjjGcUDIM3pP9cFox_We0ijPXfkuUjItpaK9xMdXoKEsSgWI6pgGTlm_CTQ0ZrLYlyztYRswMDu5ez4_FmPOvauBD7wjY8A7MUHDK_wE7b3.LMXcI8ewDsnSbds_WvPy2shKMbZHZU3vqd1HBCWjaNZ71YppHk8xMxfuvzbRqReFy6W1SI1i4o_T3Nw5BdLMxLRGd47uk',
+    'pplx.metadata': '{%22qc%22:34%2C%22qcu%22:277%2C%22qcm%22:79%2C%22qcc%22:272%2C%22qcr%22:9%2C%22qcdr%22:6%2C%22qcs%22:6%2C%22qcd%22:0%2C%22hli%22:true%2C%22hcga%22:true%2C%22hcds%22:false%2C%22hso%22:false%2C%22hfo%22:false}',
+    'AWSALB': 'rY66FXtK+RY5/b/YJ+zQhgH1eoIKmD6YO42P+I2o/2zSqLfnOviKDV5viV6kqlqA7XaA0i1SozWDPbKaqw+7IQT9Y8nk0tnjgynYatb3drDI+NwkK+C9FZs5DPDT',
+    'AWSALBCORS': 'rY66FXtK+RY5/b/YJ+zQhgH1eoIKmD6YO42P+I2o/2zSqLfnOviKDV5viV6kqlqA7XaA0i1SozWDPbKaqw+7IQT9Y8nk0tnjgynYatb3drDI+NwkK+C9FZs5DPDT',
+    '__Secure-next-auth.session-token': 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..X5DwSNasyWjAEuf_.fS4hbj37nWVSNn7YX4PwON56MJ3gxYqK4z_vLfoD_JkLSvwmvnhndhXFBwk7ONhFSVZ584urTqhWz9pvCoi20G4loBk72859d5ygVlK-QiU_YHfhOc2zZwIauXIuN4n8bknToW5771LLRqSer0_9fkvrlBoToJOxmRwtGECYd0lGdLuMLehReHYXVjXkwGqkc73cCIY9lhEcLiahT_nDra-R_-WYXV_1B-LblSul1BctWZD8UwauDEsKXSCZJ6IFv1DvSw.EskZ17CGQh9zLXt7trOsUg',
+    'lat': '31.208585357531344',
+    'long': '121.58339532266106',
+    '_dd_s': 'aid=ee23e295-8c04-4834-acbd-8b25ee81ade7&rum=0&expire=1749792350087&logs=0',
+}
+
+perplexity_cli = perplexity.Client(perplexity_cookies)
+
+# mode = ['auto', 'pro', 'reasoning', 'deep research']
+# model = model for mode, which can only be used in own accounts, that is {
+#     'auto': [None],
+#     'pro': [None, 'sonar', 'gpt-4.5', 'gpt-4o', 'claude 3.7 sonnet', 'gemini 2.0 flash', 'grok-2'],
+#     'reasoning': [None, 'r1', 'o3-mini', 'claude 3.7 sonnet'],
+#     'deep research': [None]
+# }
+# sources = ['web', 'scholar', 'social']
+# files = a dictionary which has keys as filenames and values as file data
+# stream = returns a generator when enabled and just final response when disabled
+# language = ISO 639 code of language you want to use
+# follow_up = last query info for follow-up queries, you can directly pass response from a query, look at second example below
+# incognito = Enables incognito mode, for people who are using their own account
+resp = perplexity_cli.search('hello world', mode='pro', model='claude 3.7 sonnet', sources=['web'], files={}, stream=False, follow_up=None, incognito=False)
+print(resp)
+
+# second example to show how to use follow-up queries and stream response
+# for i in perplexity_cli.search('Your query here', stream=True, follow_up=resp):
+#     print(i)
